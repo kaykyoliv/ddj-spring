e113a863d705d6e3e497c4b726ae0f703e8a9c58
Kayky Oliveira <kaykydeeoliveira@gmail.com>
2025-04-14 10:36:07 -0300

    profiles and environment variables

--- anime-service/src/main/java/com/kayky/config/Connection.java
+++ anime-service/src/main/java/com/kayky/config/Connection.java
@@ -1,10 +1,12 @@
-package external.connection;
+package com.kayky.config;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.ToString;
 
 @ToString
 @AllArgsConstructor
+@Getter
 public class Connection {
     private String host;
     private String username;
--- anime-service/src/main/java/com/kayky/repository/ProducerHardCodedRepository.java
+++ anime-service/src/main/java/com/kayky/repository/ProducerHardCodedRepository.java
@@ -1,14 +1,10 @@
 package com.kayky.repository;
 
 import com.kayky.domain.Producer;
-import external.connection.Connection;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
