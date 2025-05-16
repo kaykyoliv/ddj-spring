97117fcf21e76ca217be9e21a9ce576d9a46c32e
kaykyoliv <kaykydeeoliveira@gmail.com>
2025-05-16 10:03:09 -0300

    EntityGraph

--- user-service/src/main/java/com/kayky/repository/UserProfileRepository.java
+++ user-service/src/main/java/com/kayky/repository/UserProfileRepository.java
@@ -2,7 +2,14 @@ package com.kayky.repository;
 
 
 import com.kayky.domain.UserProfile;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+
+    //    @EntityGraph(attributePaths = {"user","profile"})
+    @EntityGraph(value = "UserProfile.fullDetails")
+    List<UserProfile> findAll();
 }
