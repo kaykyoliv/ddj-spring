729f197a861e621c5329c344ec7d70beb9c35e0e
kaykyoliv <kaykydeeoliveira@gmail.com>
2025-05-22 08:59:13 -0300

    springdoc openapi

--- user-service/src/main/java/com/kayky/controller/UserController.java
+++ user-service/src/main/java/com/kayky/controller/UserController.java
@@ -1,15 +1,24 @@
 package com.kayky.controller;
 
+import com.kayky.exception.ApiError;
+import com.kayky.exception.DefaultErrorMessage;
 import com.kayky.mapper.UserMapper;
 import com.kayky.request.UserPostRequest;
 import com.kayky.request.UserPutRequest;
 import com.kayky.response.UserGetResponse;
 import com.kayky.response.UserPostResponse;
 import com.kayky.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,11 +28,19 @@ import java.util.List;
 @RequestMapping("v1/users")
 @Slf4j
 @RequiredArgsConstructor
+@Tag(name = "User API", description = "User related endpoints")
 public class UserController {
     private final UserService service;
     private final UserMapper mapper;
 
     @GetMapping
+    @Operation(summary = "Get all users", description = "Get all users available in the system",
+            responses = {
+                    @ApiResponse(description = "List all users",
+                            responseCode = "200",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = UserGetResponse.class)))
+                    )
+            })
     public ResponseEntity<List<UserGetResponse>> findAll(@RequestParam(required = false) String firstName) {
         log.debug("Request received to list all users, param first name '{}'", firstName);
 
@@ -36,6 +53,17 @@ public class UserController {
 
 
     @GetMapping("{id}")
+    @Operation(summary = "Get user by id",
+            responses = {
+                    @ApiResponse(description = "Get user by its id",
+                            responseCode = "200",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = UserGetResponse.class))
+                    ),
+                    @ApiResponse(description = "User Not Found",
+                            responseCode = "404",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DefaultErrorMessage.class))
+                    )
+            })
     public ResponseEntity<UserGetResponse> findById(@PathVariable Long id) {
         log.debug("Request to find user by id: {}", id);
 
@@ -46,6 +74,17 @@ public class UserController {
         return ResponseEntity.ok(userGetResponse);
     }
 
+    @Operation(summary = "Create user",
+            responses = {
+                    @ApiResponse(description = "Save user in the database",
+                            responseCode = "201",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = UserPostResponse.class))
+                    ),
+                    @ApiResponse(description = "Bad Request",
+                            responseCode = "400",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiError.class))
+                    )
+            })
     @PostMapping
     public ResponseEntity<UserPostResponse> save(@RequestBody @Valid UserPostRequest request) {
         log.debug("Request to save user : {}", request);
