4ebdc98fbb7bd5d477db1fe2ddd25e34902cecc8
kaykyoliv <kaykydeeoliveira@gmail.com>
2025-05-24 08:32:33 -0300

    add spring security

---
+++ user-service/src/main/java/com/kayky/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.kayky.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetails(PasswordEncoder passwordEncoder){
+        var user = User.withUsername("takamura")
+                .password(passwordEncoder.encode("ippo"))
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("kinho"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
